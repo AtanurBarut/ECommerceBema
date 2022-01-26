a26a913f9ece8e91b101c206e643e24293195a62
Atanur Barut <atanurbarut@hotmail.com>
2022-01-26 13:59:29 +0300

    dfdsf

---
+++ DataAcccess/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAcccess
+{
+    public interface IBaseRepository<T> where T :class,IEntity,new()
+    {
+        Task<IEnumerable<T>> GetListAsync();
+    }
+}
