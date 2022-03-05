f94ea15f1a8e6f919f225b016b8efecb325c6d09
Atanur Barut <atanurbarut@hotmail.com>
2022-03-05 16:54:17 +0300

    Revert "Form"
    This reverts commit e2265244c3b7ec38e3a31d913df958833314f434.

--- WebAPIWindowsForm/Form1.cs
+++ WebAPIWindowsForm/Form1.cs
@@ -1,12 +1,9 @@
-﻿using Entities.Dtos.UserDtos;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,21 +12,9 @@ namespace WebAPIWindowsForm
 {
     public partial class Form1 : Form
     {
-        private string url = "http://localhost:11554/api/Users/GetList";
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private async void Form1_Load(object sender, EventArgs e)
-        {
-            using (HttpClient httpClient=new HttpClient())
-            {
-                var users = await httpClient.GetFromJsonAsync<List<UserDetailDto>>(new Uri(url));
-                dataGridView1.DataSource = users;
-
-            }
-
         }
     }
 }
